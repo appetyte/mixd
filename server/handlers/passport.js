0b1b0a9c944f76737cfbf1c7aba2895d3c804e56
agarun <19801205+agarun@users.noreply.github.com>
2018-03-01 18:14:32 -0500

    Implement passport support

---
+++ server/handlers/passport.js
@@ -0,0 +1,7 @@
+import passport from 'passport';
+import mongoose from 'mongoose';
+
+const User = mongoose.model('User');
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
