275ec47610ebc648b51f1d365dcb8c6a8ad2d898
agarun <19801205+agarun@users.noreply.github.com>
2018-03-01 18:15:18 -0500

    Add style support

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import Root from './Root';
 import store from './store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const RootWithStore = <Root store={store} />;
+  const RootWithStore = <Root store={store()} />;
   const rootElement = document.getElementById('root');
   ReactDOM.render(RootWithStore, rootElement);
 });
