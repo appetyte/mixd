7b97db474af6f128c8424d2d343d54b80d3cf943
Luke Espina <luke.espina@gmail.com>
2018-03-05 15:48:18 -0500

    Add apiCaller file and mixable actions

---
+++ client/modules/Mixable/mixableActions.js
@@ -0,0 +1,15 @@
+import apiCaller from "apiCaller";
+
+export const RECEIVE_MIXABLE = "RECEIVE_MIXABLE";
+
+export const receiveMixable = mixable => ({
+  type: RECEIVE_MIXABLE,
+  mixable
+});
+
+export const fetchMixable = mixableId => dispatch => {
+  return apiCaller(`api/mixables/${mixableId}`, "get").then(mixable => {
+    dispatch(receiveMixable(mixable));
+    return mixable;
+  });
+};
---
+++ client/util/apiCaller.js
@@ -0,0 +1,15 @@
+import fetch from "isomorphic-fetch";
+import Config from "../../server/config";
+
+export const API_URL =
+  typeof window === "undefined" || process.env.NODE_ENV === "test"
+    ? `http://localhost:${process.env.PORT || Config.port}/api`
+    : "/api";
+
+export default function callApi(endpoint, method = "get", body) {
+  return fetch(`${API_URL}/${endpoint}`, {
+    headers: { "content-type": "application/json" },
+    method,
+    body: JSON.stringify(body)
+  });
+}
