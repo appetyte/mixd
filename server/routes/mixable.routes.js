624c328fb857e30b2166de0d14fdfea25ce6533f
Tim Kwak <tim.k.kwak@gmail.com>
2018-03-05 13:21:34 -0500

    Add Mixable model routes and controller

---
+++ server/routes/mixable.routes.js
@@ -0,0 +1,11 @@
+import {
+  Router
+} from 'express';
+import * as MixableController from '../controllers/mixable.controller';
+
+const router = new Router();
+
+router.route('/mixables/from_shelf')
+  .get(MixableController.fromShelf);
+
+export default router;
