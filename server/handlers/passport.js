dfc4f71a22d5d22d10fa9242238ff59804021815
agarun <19801205+agarun@users.noreply.github.com>
2018-03-06 14:06:56 -0500

    Add google strategy

--- server/handlers/passport.js
+++ server/handlers/passport.js
@@ -1,7 +1,31 @@
 import passport from 'passport';
 import mongoose from 'mongoose';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 const User = mongoose.model('User');
+
 passport.use(User.createStrategy());
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/api/auth/google/callback',
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    },
+  ),
+);
