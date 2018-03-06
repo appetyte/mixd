9f66fbc8921a8ddd933f3b8308813f0754403d4d
agarun <19801205+agarun@users.noreply.github.com>
2018-03-05 23:31:21 -0500

    Fix app component

--- client/App.js
+++ client/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import './app.scss';
 import LoginForm from './modules/Session/components/LoginForm';
 import SignupForm from './modules/Session/components/SignupForm';
 
@@ -8,8 +9,8 @@ const App = () => (
   <main>
     Test string #1.
     <Switch>
-      <Route path="/login" component={LoginForm} />
-      <Route path="/signup" component={SignupForm} />
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/signup" component={SignupForm} />
     </Switch>
   </main>
 );
