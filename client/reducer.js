31fc867710400930d3c68d7929cd106d7d1af800
agarun <19801205+agarun@users.noreply.github.com>
2018-03-05 23:08:12 -0500

    Update root reducer

--- client/reducer.js
+++ client/reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import session from './modules/Session/SessionReducer';
 
 const rootReducer = combineReducers({
-
+  session,
 });
 
 export default rootReducer;
