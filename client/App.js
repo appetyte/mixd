8cba2ec1c8399b0b9fd0f941a37a00348c1e778b
agarun <19801205+agarun@users.noreply.github.com>
2018-03-07 12:13:54 -0500

    Integrate Header

--- client/App.js
+++ client/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './base.scss';
+import Header from './modules/Header/components/Header';
 import LoginForm from './modules/Session/components/LoginForm';
 import SignupForm from './modules/Session/components/SignupForm';
 
 const App = () => (
   <main>
+    <Header />
     <Switch>
       <Route exact path="/login" component={LoginForm} />
       <Route exact path="/signup" component={SignupForm} />
--- client/routes.js
+++ client/routes.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import App from './App';
+import Header from './modules/Header/components/Header';
 import LoginForm from './modules/Session/components/LoginForm';
 import SignupForm from './modules/Session/components/SignupForm';
 
 export default [
   { component: App,
     routes: [
+      {
+        path: '/',
+        component: Header,
+      },
       { path: '/login',
         exact: true,
         component: LoginForm
