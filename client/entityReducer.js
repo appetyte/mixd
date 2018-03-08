7028bc0e824fc6238129b0385855068ea16173ad
Tim Kwak <tim.k.kwak@gmail.com>
2018-03-08 16:01:14 -0500

    Add graphElements

--- client/entityReducer.js
+++ client/entityReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import mixables from "./modules/Mixable/mixableReducer";
+import graphElements from "./modules/Graph/graphElementReducer";
 
 const entityReducer = combineReducers({
-  mixables
+  mixables,
+  graphElements
 });
 
 export default entityReducer;
