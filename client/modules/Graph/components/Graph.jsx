f74562716dca61b789bb7c2c55bd1eeabdf8eab6
Tim Kwak <tim.k.kwak@gmail.com>
2018-03-08 19:16:46 -0500

    Refactor

--- client/modules/Graph/components/Graph.jsx
+++ client/modules/Graph/components/Graph.jsx
@@ -17,6 +17,126 @@ class Graph extends React.Component {
     if(this.props.nodes.length > 0) this.updateGraph(this);
   }
 
+  getRadialForceStrength(d) {
+    if (d.type === 'I') {
+      if (d.inShelf) {
+        return 0.1;
+      } else {
+        return 0.1;
+      }
+    } else {
+      return 0.1;
+    }
+  }
+
+  getRadialForce(d) {
+    if (d.type === 'I') {
+      if (d.inShelf) {
+        return 0;
+      } else {
+        return 300;
+      }
+    }
+    else {
+      return 200;
+    }
+  }
+
+  getNodeRadius(d) {
+    return 10 + d.recipesCount * 0.75;
+  }
+
+  getNodeColor(d) {
+    if (d.type === 'I') {
+      if (d.inShelf) {
+        return '#4389f9';
+      } else {
+        return '#94bcfc';
+      }
+    }
+    else {
+      switch (d.category) {
+        case "Ordinary Drink":
+          return "#ff7200";
+        case "Cocktail":
+          return "#ff1900";
+        case "Other/Unknown":
+          return "#d0ff00";
+        default:
+          return "#ff7200";
+      }
+    }
+  }
+
+  simTick(){
+    node.attr('cx', d => d.x).attr('cy', d => d.y);
+    link
+      .attr('x1', d => d.source.x).attr('y1', d => d.source.y)
+      .attr('x2', d => d.target.x).attr('y2', d => d.target.y);
+  }
+
+  handleZoom() {
+    graph.attr("transform", d3.event.transform);
+  }
+
+  nodeMouseOver(d) {
+    tooltip.transition()
+      .duration(200)
+      .style("opacity", .9);
+    tooltip.html(d.name)
+      .style("left", (d3.event.pageX) + "px")
+      .style("top", (d3.event.pageY - 28) + "px");
+  }
+
+  nodeClick(d) {
+    d3.selectAll('.links line')
+      .attr('class', link => {
+        if (link.source === d || link.target === d) {
+          return "highlight";
+        } else {
+          return "";
+        }
+      });
+    if (d.type === "R" || d.type === "RI") {
+      component.props.openModal();
+      component.props.showMixable(d.name);
+    }
+  }
+
+  removeHighlights() {
+    d3.selectAll('.nodes circle').attr('class', '');
+    d3.selectAll('.links line').attr('class', '');
+  }
+
+  highlight(d) {
+    // d3.select(d).attr('class', 'highlight')
+    // console.log(d3.select(d))
+    // node.attr('class', "highLight");
+  }
+
+  nodeMouseOut(d) {
+    tooltip.transition()
+      .duration(500)
+      .style("opacity", 0);
+  }
+
+  dragStart(d) {
+    simulation.alphaTarget(1).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  dragDrag(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  dragEnd(d) {
+    simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
   updateGraph(component) {
     const svg = d3.select('svg');
     const tooltip = d3.select('.graph__tooltip');
@@ -55,7 +175,7 @@ class Graph extends React.Component {
       .on("mouseout", nodeMouseOut)
       .on("click", nodeClick);
 
-    simulation.alphaTarget(0.2);
+    // simulation.alphaTarget(0.2);
     simulation.on('tick', simTick);
 
     // User Input Handlers
